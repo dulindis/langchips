3505051c5ef9546a4add0406f8e2ac90956902d3
POkulska <paulina.okulska@gmail.com>
2025-02-09 19:59:11 +0100

    initial setup class library and console app

---
+++ ClassLibrary/Models/Expression.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class Expression
+    {
+        int Id;
+        public string ExpressionText;
+        string LanguageCode;
+        public Expression(string expression_text, string language_code)
+        {
+            ExpressionText = expression_text;
+            LanguageCode = language_code;
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassLibrary.Models.Expression expression = new ClassLibrary.Models.Expression("test word", "pl");
+            Console.WriteLine("Hello, Langchips!" + expression.ExpressionText);
+        }
+    }
+}
