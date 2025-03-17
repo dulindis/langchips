24d5cf171cda994f4cb9ae243c3a5d32ebf66694
POkulska <paulina.okulska@gmail.com>
2025-03-17 19:50:25 +0100

    Created Configuration Servie tests

---
+++ Langchips.Tests/ConfigurationServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Langchips.Services;
+using Microsoft.Extensions.Configuration;
+namespace Langchips.Tests;
+
+public class ConfigurationServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GetConnectionString_ReturnsExpectedConnectionString()
+    {
+        var inMemorySettings = new Dictionary<string, string> {
+                {"ConnectionStrings:DefaultConnection", "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;"}
+            };
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+
+        var configurationService = new ConfigurationService(configuration);
+
+        var connectionString = configurationService.GetConnectionString("DefaultConnection");
+
+        Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", connectionString);
+    }
+
+    [Test]
+    public void GetConnectionString_ThrowsException_WhenConnectionStringIsMissing()
+    {
+        IConfiguration configuration = new ConfigurationBuilder().Build();
+        var configurationService = new ConfigurationService(configuration);
+
+        Assert.Throws<InvalidOperationException>(() => configurationService.GetConnectionString("DefaultConnection"));
+    }
+}
