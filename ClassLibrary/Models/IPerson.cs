81d3981d321d8a6e7361449efd8481b8ce7a8ef2
POkulska <paulina.okulska@gmail.com>
2025-03-20 10:11:55 +0100

    adding interface for a person

---
+++ ClassLibrary/Models/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Langchips.Models.Models
+{
+    internal interface IPerson
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
